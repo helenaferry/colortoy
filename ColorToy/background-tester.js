aa2f7b8116b118893420dba2fcbca903bf34ecda
helenaferry <helena.ferry@gmail.com>
2014-06-07 16:17:28 +0200

    jquery, bootstrap

---
+++ ColorToy/background-tester.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#get-url").draggable();
+
+    $("#bg-url").on("change", function () {
+        $("#url").html("<a href='" + this.value + "'>" + this.value + "</a>");
+        $("body").css("background", "url('" + this.value + "')");
+    });
+});
